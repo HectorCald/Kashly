ebd4e7acfc783062074ed2fd84451fcbb51fa86a
Hector Cald <hi.hector20@gmail.com>
2025-07-05 11:44:22 -0400

    cat-etiq overlay2.1

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'kashly-v17';
-const STATIC_CACHE = 'kashly-static-v17';
+const CACHE_NAME = 'kashly-v18';
+const STATIC_CACHE = 'kashly-static-v18';
 
 // Recursos críticos que se cachean inmediatamente
 const STATIC_RESOURCES = [
