182c3199eb5592ada7ea7e05f3a484820934de35
Hector Cald <hi.hector20@gmail.com>
2025-07-05 12:39:00 -0400

    retroceso

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'kashly-v1';
-const STATIC_CACHE = 'kashly-static-v1';
+const CACHE_NAME = 'kashly-v3';
+const STATIC_CACHE = 'kashly-static-v3';
 
 // Recursos críticos que se cachean inmediatamente
 const STATIC_RESOURCES = [
